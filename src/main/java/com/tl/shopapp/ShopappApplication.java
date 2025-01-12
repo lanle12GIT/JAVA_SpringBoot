b5fcb197d5d7c45b3abdc9da01a806fc917e1204
Lan Le <lanle120801@gmail.com>
2025-01-12 16:48:55 +0700

    init

---
+++ src/main/java/com/tl/shopapp/ShopappApplication.java
@@ -0,0 +1,13 @@
+package com.tl.shopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopappApplication.class, args);
+	}
+
+}
