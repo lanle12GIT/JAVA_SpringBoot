ab97595e82024c4d714a7927cad22e16a7433416
Lan Le <lanle120801@gmail.com>
2025-01-14 21:40:10 +0700

    add db

---
+++ database.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE ShopAppLan;
+USE ShopAppLan;
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(100) DEFAULT '',
+    phone_number VARCHAR(10) NOT NULL,
+    address VARCHAR(200) DEFAULT '',
+    password VARCHAR(100) NOT NULL DEFAULT '',-- mat khau da ma hoa
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_active TINYINT(1) DEFAULT 1,
+    date_of_birth DATE,
+    facebook_account_id INT DEFAULT 0,
+    google_account_id INT DEFAULT 0
+);
+ALTER TABLE users ADD COLUMN role_id INT;
+CREATE TABLE roles(
+    id INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL 
+);
+ALTER TABLE users ADD FOREIGN KEY (role_id)REFERENCES roles(id);
+CREATE TABLE tokens(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    token_type VARCHAR(50) NOT NULL,
+    expiration_date DATETIME,
+    revoked TINYINT(1) NOT NULL,
+    expired TINYINT(1) NOT NULL,
+    user_id INT,note VARCHAR(100) DE
+    FOREIGN KEY ( user_id) REFERENCES users(id)
+);
+-- ho tro dang nhap tu Face or GG
+CREATE TABLE social_accounts(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    provider VARCHAR(20) NOT NULL,
+    provider_id VARCHAR(50) NOT NULL ,
+    email VARCHAR(150) NOT NULL,
+    name VARCHAR(100)NOT NULL,
+    user_id INT,
+    FOREIGN KEY ( user_id) REFERENCES users(id)
+
+);
+-- bang danh muc san pham
+CREATE TABLE categories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL DEFAULT''
+);
+-- bang chua san pham
+CREATE TABLE products(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(350),
+    price FLOAT NOT NULL CHECK(price>=0),
+    thumbanail VARCHAR(300) DEFAULT'',
+    description LONGTEXT DEFAULT'',
+    created_at DATETIME,
+    updated_at DATETIME,
+    category_id INT
+    FOREIGN KEY ( category_id) REFERENCES categories(id)
+);
+-- dat hang
+CREATE TABLE orders(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    FOREIGN KEY ( user_id) REFERENCES users(id),
+    fullname VARCHAR(100),
+    email VARCHAR(100),
+    phone_number VARCHAR(20) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    FAULT'',
+    orders_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(20),
+    total_money FLOAT CHECK(total_money>=0)
+);
+ALTER TABLE orders ADD COLUMN shipping_method VARCHAR(100);
+ALTER TABLE orders ADD COLUMN shipping_address VARCHAR(200);
+ALTER TABLE orders ADD COLUMN shipping_date DATE;
+ALTER TABLE orders ADD COLUMN shipping_number VARCHAR(100);
+ALTER TABLE orders ADD COLUMN payment_method VARCHAR(100);
+ALTER TABLE orders ADD COLUMN active TINYINT(1);
+ALTER TABLE orders
+MODIFY COLUMN status ENUM('pending','processing', 'shipped', 'delivered','cancelled')
+CREATE TABLE orders_details(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    orders_id INT
+    FOREIGN KEY ( order_id) REFERENCES orders(id),
+    product_id INT
+    FOREIGN KEY ( product_id) REFERENCES products(id),
+    price FLOAT CHECK(price>=0),
+    number_of_products INT CHECK(number_of_products>0),
+    total_money FLOAT CHECK(total_money>=0),
+    color VARCHAR(20) DEFAULT '',
+    );
